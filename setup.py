d4387875d40b3d249eb988c8c4a8af1f3d640ad0
Aria Vesta <dev@ariavesta.com>
2019-10-08 01:21:28 -0600

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     entry_points={'console_scripts': ['glotter = glotter.__main__:main']},
     packages=find_packages(exclude=('test',)),
     install_requires=[
-        'docker>=3.7.0, <3.8',
+        'docker>=4.1.0, <4.2',
         'Jinja2>=2.10.1, <2.11',
-        'pytest>=4.3.1, <4.4',
+        'pytest>=5.2.1, <5.3',
         'PyYAML>=5.1, <5.2'
     ],
     python_requires='>=3.5',
