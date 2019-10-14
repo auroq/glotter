4e9a6f3721b6bdd67e719c1233bab75b68f279a4
Aria Vesta <dev@ariavesta.com>
2019-10-14 06:08:46 -0600

    Fix bug with naming scheme

--- glotter/project.py
+++ glotter/project.py
@@ -20,7 +20,7 @@ class Project:
         self._words = words
         self._requires_parameters = requires_parameters
         self._acronyms = [acronym.upper() for acronym in acronyms] if acronyms else []
-        self._acronym_scheme = acronym_scheme or AcronymScheme.upper
+        self._acronym_scheme = acronym_scheme or AcronymScheme.two_letter_limit
 
     @property
     def words(self):
@@ -57,16 +57,17 @@ class Project:
             raise KeyError(f"Unknown naming scheme '{naming}'", e)
 
     def _as_hyphen(self):
-        return '-'.join(map(self._try_as_acronym, self._words))
+        return '-'.join([self._try_as_acronym(word, NamingScheme.hyphen) for word in self._words])
 
     def _as_underscore(self):
-        return '_'.join(map(self._try_as_acronym, self._words))
+        return '_'.join([self._try_as_acronym(word, NamingScheme.underscore) for word in self._words])
 
     def _as_camel(self):
-        return self._words[0].lower() + ''.join([self._try_as_acronym(word.title()) for word in self._words[1:]])
+        return self._words[0].lower() + ''.join([self._try_as_acronym(word.title(), NamingScheme.camel)
+                                                 for word in self.words[1:]])
 
     def _as_pascal(self):
-        return ''.join([self._try_as_acronym(word.title()) for word in self._words])
+        return ''.join([self._try_as_acronym(word.title(), NamingScheme.pascal) for word in self._words])
 
     def _as_lower(self):
         return ''.join([word.lower() for word in self._words])
@@ -74,19 +75,19 @@ class Project:
     def _is_acronym(self, word):
         return word.upper() in self._acronyms
 
-    def _try_as_acronym(self, word):
+    def _try_as_acronym(self, word, naming_scheme):
         if self._is_acronym(word):
             if self._acronym_scheme == AcronymScheme.upper:
                 return word.upper()
             elif self._acronym_scheme == AcronymScheme.lower:
                 return word.lower()
             elif self._acronym_scheme == AcronymScheme.two_letter_limit:
-                if len(word) <= 2:
+                if len(word) <= 2 and naming_scheme in [NamingScheme.camel, NamingScheme.pascal]:
                     return word.upper()
         return word
 
     def __eq__(self, other):
-        return self._words == other._words and \
-               self._requires_parameters == other._requires_parameters and \
-               self._acronyms == other._acronyms and \
-               self._acronym_scheme == other._acronym_scheme
+        return self._words == other.words and \
+               self._requires_parameters == other.requires_parameters and \
+               self._acronyms == other.acronyms and \
+               self._acronym_scheme == other.acronym_scheme
